21e6864c5827be2779b796a2e7733956d1d15d70
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-07 19:38:49 +0100

    Update shopify-config.js

--- shopify-config.js
+++ shopify-config.js
@@ -1,44 +1,12 @@
 // shopify-config.js
 
-import pkg from "@shopify/shopify-api";
-import nodeAdapter from "@shopify/shopify-api/adapters/node.js";
-
-const { shopifyApi, LATEST_API_VERSION } = pkg;
-
-// Set the Node.js adapter
-pkg.shopifyApi.adapters.set(nodeAdapter);
-
-class CustomMemoryStorage {
-  constructor() {
-    this.sessions = new Map();
-  }
-
-  async storeSession(session) {
-    this.sessions.set(session.id, session);
-    return true;
-  }
-
-  async loadSession(id) {
-    return this.sessions.get(id) || undefined;
-  }
-
-  async deleteSession(id) {
-    return this.sessions.delete(id);
-  }
-
-  async findSessionsByShop(shop) {
-    return Array.from(this.sessions.values()).filter(
-      (session) => session.shop === shop
-    );
-  }
-}
+import { shopifyApi, LATEST_API_VERSION } from "@shopify/shopify-api";
 
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   scopes: process.env.SCOPES.split(","),
-  hostName: process.env.HOST.replace(/^https?:\/\//, ""),
-  isEmbeddedApp: true,
+  hostName: new URL(process.env.HOST).host,
   apiVersion: LATEST_API_VERSION,
-  sessionStorage: new CustomMemoryStorage(),
+  isEmbeddedApp: false,
 });
