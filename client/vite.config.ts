5351380245f66a2bb85447cfeec496a397626260
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-07 23:59:08 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  root: "client",
-  build: { outDir: "../public/app", emptyOutDir: true },
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
 });
