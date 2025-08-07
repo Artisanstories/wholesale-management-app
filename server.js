466131b241b9ad7b9d92e5f77ea490e3d476dfbb
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-07 17:23:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { shopify } from "./shopify-config.js";
-import { applyAuthMiddleware } from "./auth.js";
+import shopify from "./shopify-config.js";
+import applyAuthMiddleware from "./auth.js";
+import addScriptTag from "./script-injector.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 applyAuthMiddleware(app);
 
