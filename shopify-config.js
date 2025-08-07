f8941632a42a972f6d838e00bc3f8747a4769cb7
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-07 17:32:31 +0100

    Update shopify-config.js

--- shopify-config.js
+++ shopify-config.js
@@ -1,14 +1,14 @@
-// shopify-config.js
 import pkg from "@shopify/shopify-api";
 const { shopifyApi, LATEST_API_VERSION } = pkg;
 
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
-  scopes: process.env.SCOPES?.split(","),
+  scopes: process.env.SCOPES.split(","),
   hostName: new URL(process.env.HOST).host,
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: false,
 });
 
 export default shopify;
+
