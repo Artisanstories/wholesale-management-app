fdc40b80146dddf2ce45a5f07944d46d23d0d239
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-09 23:25:27 +0100

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@shopify/polaris/build/esm/styles.css";
 import "./app.css";
+
 import App from "./App";
-import TopBarProvider from "./components/TopBarProvider";
+
+import {AppProvider} from "@shopify/polaris";
+import en from "@shopify/polaris/locales/en.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TopBarProvider>
+    <AppProvider i18n={en}>
       <App />
-    </TopBarProvider>
+    </AppProvider>
   </React.StrictMode>
 );
