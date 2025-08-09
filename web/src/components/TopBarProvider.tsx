19c0b8da5a79e2b625486682368d13b26d309526
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-09 23:21:47 +0100

    Update TopBarProvider.tsx

--- web/src/components/TopBarProvider.tsx
+++ web/src/components/TopBarProvider.tsx
@@ -1,11 +1,13 @@
-import { AppBridgeProvider } from "@shopify/app-bridge-react";
+import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
 import { ReactNode, useMemo } from "react";
 
 export default function TopBarProvider({ children }: { children: ReactNode }) {
   const config = useMemo(() => {
-    const host = new URLSearchParams(location.search).get("host") || window.__SHOPIFY_HOST;
+    const host =
+      new URLSearchParams(location.search).get("host") || window.__SHOPIFY_HOST;
     const apiKey = import.meta.env.VITE_SHOPIFY_API_KEY as string;
     return { host, apiKey, forceRedirect: true };
   }, []);
+
   return <AppBridgeProvider config={config}>{children}</AppBridgeProvider>;
 }
