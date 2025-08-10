2b4553be05edef8af959a874edf9bbf4fcd0aa19
Artisanstories <info@artisanstorieslondon.co.uk>
2025-08-10 13:10:00 +0100

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,8 @@
-// web/vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  server: { host: true, port: 5173 },
+  preview: { port: 4173 }
 });
